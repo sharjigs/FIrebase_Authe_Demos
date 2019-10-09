d3e59844ad399bff434f8287ba4ffd97afe3d1fe
Jignesh Sharma <Jignesh.Sharma@gatewaygroup.com>
2019-10-09 17:26:34 +0530

    Signup code Added

--- scripts/auth.js
+++ scripts/auth.js
@@ -15,31 +15,3 @@ signupForm.addEventListener('submit', (e) => {
         signupForm.reset();
     });
 });
-
-//logout
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e)=>{
-    e.preventDefault();
-    auth.signOut().then(() => {
-        console.log('User Sign Out');
-    });
-});
-
-// login
-
-const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    //get User Info
-    const email = loginForm['login-email'].value;
-    const password = loginForm['login-password'].value;
-
-    auth.signInWithEmailAndPassword(email, password).then(crd => {
-        console.log(crd.user);
-        // close the login Modal and reset the form
-        const modal = document.querySelector('#modal-login');
-        M.Modal.getInstance(modal).close();
-        loginForm.reset();
-    })
-})
