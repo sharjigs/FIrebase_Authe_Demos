3c4517db325e1b2de94d97d38811477a92823b2a
Jignesh Sharma <Jignesh.Sharma@gatewaygroup.com>
2019-10-09 18:26:16 +0530

    Logout Functionality Added

--- scripts/auth.js
+++ scripts/auth.js
@@ -15,3 +15,12 @@ signupForm.addEventListener('submit', (e) => {
         signupForm.reset();
     });
 });
+
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e)=>{
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log('User Sign Out');
+    });
+});
